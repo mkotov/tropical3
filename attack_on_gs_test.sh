c4ab64fc79cdb448aef97875caea6489134788b0
Matvei Kotov <mkotov@gradarius.com>
2023-08-07 15:13:31 +0600

    refactoring

--- attack_on_gs_test.sh
+++ attack_on_gs_test.sh
@@ -4,7 +4,7 @@ python3 attack_on_gs_test.py \
   --count=10 \
   --timeout=60 \
   --size=10 \
-  --min_matrix_elem=-100000 \
+  --min_matrix_elem=0 \
   --max_matrix_elem=100000 \
   --min_poly_deg=5 \
   --max_poly_deg=15 \
